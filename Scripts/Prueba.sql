2f9ffa62b3d07ea2b59c49083ad1bfc625f64c19
Iker <162556194+IkerOwO@users.noreply.github.com>
2026-03-10 09:10:53 +0100

    Delete Scripts directory

--- Scripts/Prueba.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS Prueba;
-CREATE DATABASE Prueba;
-USE Prueba;
-CREATE TABLE Ok (texto varchar(20), xd int, rtre blob, read text);
-CREATE TABLE xd (texto varchar(20), xd int, rtre blob, read text);
